f05ba604a7f3e1c96d6d81ac208243cb68abd0ca
AvivE <aviv7385@gmail.com>
2021-05-17 18:29:17 +0300

    06: backend - finished carts logic and controller

--- Backend/business-logic-layer/carts-logic.js
+++ Backend/business-logic-layer/carts-logic.js
@@ -12,15 +12,25 @@ async function addNewCartAsync(cart) {
 // add new item to the cart
 async function addNewItemAsync(item) {
     const sql = `INSERT INTO cartitems VALUES(DEFAULT, ?, ?, ?, ?)`;
-    const values = [item.itemId, item.productId, item.cartId, item.quantity, item.totalPrice];
+    const values = [item.productId, item.cartId, item.quantity, item.totalPrice];
     const info = await dal.executeAsync(sql, values);
     item.itemId = info.insertId;
     return item;
 }
 
-// display all cart items 
+// // get all carts 
+// async function getAllCartsAsync() {
+//     const sql = `SELECT SC.cartId, SC.userId, SC.date, SUM(CI.totalPrice) AS totalCartPrice 
+//                 FROM shoppingcarts AS SC LEFT JOIN cartitems AS CI
+//                 ON SC.cartId=CI.cartId
+//                 GROUP BY SC.cartId`;
+//     const carts = await dal.executeAsync(sql);
+//     return carts;
+// }
+
+// get all cart items 
 async function getAllCartItemsAsync(cartId) {
-    const sql = `SELECT P.productName, P.price, P.imageFileName, C.cartId, C.quantity, C.totalPrice 
+    const sql = `SELECT P.productName, P.price, P.imageFileName, C.cartId, C.quantity, (C.quantity*P.price) AS totalPrice 
                 FROM cartitems AS C JOIN products AS P
                 ON C.productId=P.productId
                 WHERE C.cartId=${cartId}`;
@@ -30,17 +40,32 @@ async function getAllCartItemsAsync(cartId) {
 
 // get the total price of all items in a cart
 async function getTotalPriceAsync(cartId) {
-    const sql = `SELECT SUM(totalPrice) AS totalCartPrice
-                FROM cartitems WHERE cartId=${cartId}`;
+    const sql = `SELECT SUM(C.quantity*P.price) AS totalCartPrice
+                FROM cartitems AS C JOIN products AS P 
+                ON C.productId=P.productId
+                WHERE cartId=${cartId}`;
     const totalCartPrice = await dal.executeAsync(sql);
     return totalCartPrice;
 }
 
+// delete an item from a cart
+async function deleteCartItemAsync(cartId, itemId) {
+    const sql = `DELETE FROM cartitems WHERE cartId=${cartId} AND itemId=${itemId}`;
+    await dal.executeAsync(sql);
+}
 
+// partial update - change the quantity of a specific item in a specific cart
+async function updateQuantityOfItem(item) {
+    const sql = `UPDATE cartitems SET quantity=${item.quantity} WHERE itemId=${item.itemId}`;
+    const info = await dal.executeAsync(sql);
+    return info.affectedRows === 0 ? null : item;
+}
 
 module.exports = {
     addNewCartAsync,
     addNewItemAsync,
     getAllCartItemsAsync,
-    getTotalPriceAsync
+    getTotalPriceAsync,
+    deleteCartItemAsync,
+    updateQuantityOfItem
 }
--- Backend/controllers/carts-controller.js
+++ Backend/controllers/carts-controller.js
@@ -16,6 +16,17 @@ router.post("/", async (request, response) => {
     }
 });
 
+// // GET - get all carts (http://localhost:3001/api/carts) ** LoggedIn User access only **
+// router.get("/", async (request, response) => {
+//     try {
+//         const carts = await cartsLogic.getAllCartsAsync();
+//         response.json(carts);
+//     }
+//     catch (err) {
+//         response.status(500).send(err.message);
+//     }
+// })
+
 // POST - add one item to the cart (http://localhost:3001/api/carts/items) ** LoggedIn User access only **
 router.post("/items", async (request, response) => {
     try {
@@ -56,5 +67,37 @@ router.get("/total-price/:cartId", async (request, response) => {
     }
 });
 
+
+// DELETE - remove one item from a specific cart (http://localhost:3001/api/carts/items/6/12) ** LoggedIn User access only **
+router.delete("/items/:cartId/:itemId", async (request, response) => {
+    try {
+        const cartId = +request.params.cartId;
+        const itemId = +request.params.itemId;
+        await cartsLogic.deleteCartItemAsync(cartId, itemId);
+        response.sendStatus(204);
+    }
+    catch (err) {
+        response.status(500).send(err.message);
+    }
+});
+
+// PATCH - update the quantity of an item (http://localhost:3001/api/carts/items-quantity/12) ** LoggedIn User access only **
+router.patch("/items-quantity/:itemId", async (request, response) => {
+    try {
+        const cartItem = request.body;
+        cartItem.itemId = +request.params.itemId;
+        const updatedCartItem = await cartsLogic.updateQuantityOfItem(cartItem);
+        console.log(updatedCartItem);
+
+        if (!updatedCartItem) {
+            response.status(404).send(`id ${updatedCartItem.itemId} not found.`);
+            return;
+        }
+        response.json(updatedCartItem);
+    }
+    catch (err) {
+        response.status(500).send(err.message);
+    }
+});
 
 module.exports = router;
